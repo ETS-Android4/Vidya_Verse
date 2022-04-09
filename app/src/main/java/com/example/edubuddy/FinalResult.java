96375c8bac557ebe108f65a17222a84e2dfbd8e6
Soumen <soumenkp2@gmail.com>
2022-04-09 09:18:07 +0530

    Added activities

---
+++ app/src/main/java/com/example/edubuddy/FinalResult.java
@@ -0,0 +1,14 @@
+package com.example.edubuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_result);
+    }
+}
